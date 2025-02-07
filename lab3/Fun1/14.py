3a2f9183ff470eb5573189a313b66ccae0f1593f
A1aulym <bosajaaulym@gmail.com>
2025-02-08 01:37:07 +0500

    1.py

---
+++ lab3/Fun1/14.py
@@ -0,0 +1,7 @@
+
+
+# 14. Import functions in a Python file and use them
+# Save this script as `functions.py` and import them in another script
+
+# In another script you would do:
+# from functions import grams_to_ounces, fahrenheit_to_celsius, solve, etc.
---
+++ lab3/Fun1/5.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+def str_per(s):
+    for p in permutations(s):
+        print (''.join(p))
+
+
+y = "str"
+str_per(y)
